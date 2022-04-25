b5e17d54d92999cf5d921ac18b9ae9b38d8eee60
judhacdc <jdxxaa@gmail.com>
2022-04-25 11:24:59 +0700

    Membuat controller TodoController dan model Todo

---
+++ app/Models/Todo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'todo',
+        'label',
+        'done',
+    ];
+
+    protected $hidden = [
+        'user_id',
+    ];
+
+    protected $casts = [
+        'done' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
