692316fa39eecc3b48a7bf9c8da796fd5d39dd66
judhacdc <jdxxaa@gmail.com>
2022-04-25 11:13:14 +0700

    Menambahkan fungsi logout pada AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
     {
         $validated = $request->validated();
 
-        if (!Auth::attempt($validated)) {
+        if(!Auth::attempt($validated)){
             return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
         }
 
@@ -47,5 +47,17 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user,
         ]);
+    }
+    public function logout()
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Tokens revoked');
+        } catch (\Throwable $e) {
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+            ));
+        }
     }
 }
