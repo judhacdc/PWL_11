bc22e639f4a5ba3c474a9a156e9c3acaaa7a6a6d
judhacdc <jdxxaa@gmail.com>
2022-04-25 09:33:21 +0700

    Menambahkan fungsi login pada AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -32,4 +32,20 @@ class AuthController extends Controller
             'user' => $user,
         ]);
     }
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        if (!Auth::attempt($validated)) {
+            return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
 }
