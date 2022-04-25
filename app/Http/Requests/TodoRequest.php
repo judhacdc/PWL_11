867f29fabcdaab449445b5e796b2d34bc9d649b1
judhacdc <jdxxaa@gmail.com>
2022-04-25 11:29:07 +0700

    mengubah parent/base class dari TodoRequest menjadi ApiRequest

--- app/Http/Requests/TodoRequest.php
+++ app/Http/Requests/TodoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TodoRequest extends FormRequest
+class TodoRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
