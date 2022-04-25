972b8713bdd0ce8ec9885bd29e371a1aac24d2f0
judhacdc <jdxxaa@gmail.com>
2022-04-25 09:20:54 +0700

    Update Tambahkan Trait ApiResponse

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponse;
 }
